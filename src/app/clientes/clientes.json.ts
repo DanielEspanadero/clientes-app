13b9b107d2eb834d177d26253466e36b63e10cdc
Daniel Españadero <despanadero@mytic.com>
2022-09-02 14:59:05 +0200

    ClientService and dependency injection created

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,14 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    { id: 1, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' },
+    { id: 2, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' },
+    { id: 3, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' },
+    { id: 4, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' },
+    { id: 5, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' },
+    { id: 6, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' },
+    { id: 7, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' },
+    { id: 8, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' },
+    { id: 9, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' },
+    { id: 10, nombre: 'Andres', apellido: 'Guzman', createAt: '2022-09-02', email: 'profesor@gmail.com' }
+  ];
