d2e84569aba02e666d720d1240d8d1b1ac9e9c6d
Daniel Españadero <despanadero@mytic.es>
2022-09-05 12:47:05 +0200

    bugs fixed

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@angular/core';
 // import { CLIENTES } from './clientes.json';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { Cliente } from './cliente';
-import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-
 
 @Injectable({
   providedIn: 'root',
 })
-  
 export class ClienteService {
-
   private urlEndpoint: string = 'http://localhost:8080/api/clientes';
-  
+
+  private httpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
   constructor(private http: HttpClient) {}
 
   getClientes(): Observable<Cliente[]> {
     // return of(CLIENTES);
     return this.http.get<Cliente[]>(this.urlEndpoint);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.urlEndpoint, cliente, {headers: this.httpHeaders});
   }
 }
