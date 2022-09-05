b20f99dd0360e414e9c03f1dda21cacbfb621353
Daniel Españadero <despanadero@mytic.es>
2022-09-05 10:37:51 +0200

    Creating form client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ClienteService } from './clientes/cliente.service';
 import { ClientesComponent } from './clientes/clientes.component';
+import { FormComponent } from './clientes/form.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
@@ -22,9 +25,15 @@ const routes: Routes = [
     FooterComponent,
     DirectivaComponent,
     ClientesComponent,
+    FormComponent,
   ],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+  ],
   providers: [ClienteService],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
